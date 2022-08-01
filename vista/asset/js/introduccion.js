450c24cd1d0fba0be6a19d8e0d64b990d9820423
SulemP <sulee.ths@gmail.com>
2022-08-01 18:04:00 +0200

    Se sube proyecto.

---
+++ vista/asset/js/introduccion.js
@@ -0,0 +1,9 @@
+let seccionHome = document.getElementById("seccionHome");
+let seccionIntroduccion = document.getElementById("seccionIntroduccion");
+
+setTimeout(() => {
+  seccionHome.setAttribute("class", "invisible");
+  seccionIntroduccion.setAttribute("class", "invisible");
+  seccionHome.removeAttribute("class", "invisible");
+  seccionHome.setAttribute("class", "home");
+}, 500);
